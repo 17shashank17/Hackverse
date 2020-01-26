c89ce00547ab16e47f613f96bdc9a0ccb93eb71a
blurrrb <17aakashsingh1999@gmail.com>
2020-01-26 10:07:11 +0530

    add sos stats

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import mark_safe_location, get_all_safe_locations, get_nearest_safe_location, get_stats, mark_unsafe_location, sos_distress
+from .views import mark_safe_location, get_all_safe_locations, get_nearest_safe_location, get_stats, mark_unsafe_location, sos_distress, get_all_distress_locations
 
 urlpatterns = [
     path('mark_safe_location', mark_safe_location),
@@ -7,6 +7,6 @@ urlpatterns = [
     path('mark_unsafe_location', mark_unsafe_location),
     path('get_all_safe_locations', get_all_safe_locations),
     path('get_stats', get_stats),
-    path('sos_distress', sos_distress)
-
+    path('sos_distress', sos_distress),
+    path('get_all_distress_locations', get_all_distress_locations)
 ]
